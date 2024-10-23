492a8b8ad618aa1b4d08e39e4fa6824d6e566b5a
Jay Millena <jay.millena07@gmail.com>
2024-10-23 19:59:30 +0800

    C# Exercise - Object-Oriented Programming: Polymorphism

---
+++ Animal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Animal
+{
+    public static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        animal.MakeSound();
+
+        Dog dog = new Dog();  
+        dog.MakeSound();  
+    }
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+    
