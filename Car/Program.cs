9a6c73d8e57c2aecf64d7dfe24d7479ced0da5ca
Jay Millena <jay.millena07@gmail.com>
2024-10-23 19:39:36 +0800

    C# Exercise - Object-Oriented Programming: Encapsulation

---
+++ Car/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Car
+{
+    private string brand = "";
+    private int speed = 0;
+
+    public static void Main(string[] args)
+    {
+        Car myCar = new Car();
+        myCar.SetBrand("Toyota");
+        Console.WriteLine($"Your car brand: {myCar.GetBrand()}");
+    }
+    public void SetBrand(string carBrand)
+    {
+        brand = carBrand;
+    }
+
+    public string GetBrand()
+    {
+        return brand;
+    }
+
+    public void Accelerate()
+    {
+        speed += 10;
+    }
+}
+
